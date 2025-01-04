b2e6bbb6fad1ec69e5e060bc8b3ffe67ecdacbd0
Zgore <pierre.zwdl@outlook.fr>
2025-01-04 02:08:31 +0100

    feat(job show): add drag and drop on candidates
    Changes details here: https://github.com/Zgore14/wttj-fullstack-test/pull/1

--- assets/src/components/Candidate/index.tsx
+++ assets/src/components/Candidate/index.tsx
@@ -1,10 +1,47 @@
 import { Card } from '@welcome-ui/card'
 import { Candidate } from '../../api'
+import { useEffect, useRef, useState } from 'react'
+import { draggable, dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
+import { attachClosestEdge } from '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge';
+import { Badge } from '@welcome-ui/badge'
+
 
 function CandidateCard({ candidate }: { candidate: Candidate }) {
+  const ref = useRef(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+
+  useEffect(() => {
+    const candidateCard = ref.current;
+    if (!candidateCard) return
+
+    return combine(
+      draggable({
+        element: candidateCard,
+        getInitialData: () => ({ candidate }),
+        onDragStart: () => setDragging(true),
+        onDrop: () => setDragging(false),
+      }),
+      dropTargetForElements({
+        element: candidateCard,
+        getData: ({ input, element }) => {
+          return attachClosestEdge(candidate, {
+            input: input,
+            element: element,
+            allowedEdges: ["top", "bottom"]
+          })
+        }
+      })
+    )
+
+  }, [candidate]);
+
   return (
-    <Card mb={10}>
-      <Card.Body>{candidate.email}</Card.Body>
+    <Card mb={10} ref={ref} opacity={dragging ? 0.4 : 1}>
+      <Card.Body>
+        <Badge>{candidate.position}</Badge>
+        {candidate.email}
+      </Card.Body>
     </Card>
   )
 }
