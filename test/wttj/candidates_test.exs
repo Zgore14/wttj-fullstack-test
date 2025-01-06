593038cac81132cf49bfac1fa7b9e23913893fe6
Zgore <pierre.zwdl@outlook.fr>
2025-01-06 17:24:24 +0100

    test(api): add more unit tests for candidates update
    Testing re-ordering of candidates afeter position and status updates.

--- test/wttj/candidates_test.exs
+++ test/wttj/candidates_test.exs
@@ -45,6 +45,106 @@ defmodule Wttj.CandidatesTest do
       assert candidate.email == email
     end
 
+    test "update_candidate/2 maintains correct position ordering when moving candidate up", %{
+      job1: job1
+    } do
+      candidate1 = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate2 = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+      candidate3 = candidate_fixture(%{job_id: job1.id, status: :new, position: 2})
+
+      assert {:ok, candidate3} = Candidates.update_candidate(candidate3, %{position: 0})
+
+      [first, second, third] =
+        job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert first.id == candidate3.id
+      assert first.position == 0
+      assert second.id == candidate1.id
+      assert second.position == 1
+      assert third.id == candidate2.id
+      assert third.position == 2
+    end
+
+    test "update_candidate/2 maintains correct position ordering when moving candidate down", %{
+      job1: job1
+    } do
+      candidate1 = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate2 = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+      candidate3 = candidate_fixture(%{job_id: job1.id, status: :new, position: 2})
+
+      assert {:ok, candidate1} = Candidates.update_candidate(candidate1, %{position: 2})
+
+      [first, second, third] =
+        job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert first.id == candidate2.id
+      assert first.position == 0
+      assert second.id == candidate3.id
+      assert second.position == 1
+      assert third.id == candidate1.id
+      assert third.position == 2
+    end
+
+    test "update_candidate/2 ajdust position ordering when moving candidate in between",
+         %{
+           job1: job1
+         } do
+      candidate1 = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate2 = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+      candidate3 = candidate_fixture(%{job_id: job1.id, status: :new, position: 2})
+
+      assert {:ok, candidate1} = Candidates.update_candidate(candidate1, %{position: 1})
+
+      [first, second, third] =
+        job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert first.id == candidate2.id
+      assert first.position == 0
+      assert second.id == candidate1.id
+      assert second.position == 1
+      assert third.id == candidate3.id
+      assert third.position == 2
+    end
+
+    test "update_candidate/2 handles status changes and position", %{job1: job1} do
+      candidate1 = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+
+      candidate2 = candidate_fixture(%{job_id: job1.id, status: :interview, position: 0})
+      candidate3 = candidate_fixture(%{job_id: job1.id, status: :interview, position: 1})
+
+      assert {:ok, candidate1} =
+               Candidates.update_candidate(candidate1, %{position: 1, status: :interview})
+
+      [first, second, third] =
+        job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert first.id == candidate2.id
+      assert first.position == 0
+      assert first.status == :interview
+
+      assert second.id == candidate1.id
+      assert second.position == 1
+      assert second.status == :interview
+
+      assert third.id == candidate3.id
+      assert third.position == 2
+      assert third.status == :interview
+    end
+
+    test "update_candidate/2 with out of bound position is adapting to existing number of candidates",
+         %{job1: job1} do
+      candidate = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+
+      candidate_fixture(%{job_id: job1.id, status: :interview, position: 0})
+      candidate_fixture(%{job_id: job1.id, status: :interview, position: 1})
+
+      assert {:ok, %{position: 0}} =
+               Candidates.update_candidate(candidate, %{status: :interview, position: -1})
+
+      assert {:ok, %{position: 2}} =
+               Candidates.update_candidate(candidate, %{status: :interview, position: 999})
+    end
+
     test "update_candidate/2 with invalid data returns error changeset", %{job1: job1} do
       candidate = candidate_fixture(%{job_id: job1.id})
       assert {:error, %Ecto.Changeset{}} = Candidates.update_candidate(candidate, @invalid_attrs)
